8bcaf894a047e37acdf06bfab118b1203cf91340
Thông Trần <quangthong211101@gmail.com>
2023-09-06 15:25:09 +0700

    Create table Category and Add Foreign key in Product

--- WebBlazorEc/Shared/Product.cs
+++ WebBlazorEc/Shared/Product.cs
@@ -11,5 +11,7 @@ namespace WebBlazorEc.Shared
 
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
     }
 }
