f03fddc15a216869f08fd37f9454fcc7c1312b0b
Thông Trần <quangthong211101@gmail.com>
2023-09-06 16:22:57 +0700

    Add a CategoryService on the Client

--- WebBlazorEc/Client/Program.cs
+++ WebBlazorEc/Client/Program.cs
@@ -2,6 +2,7 @@ global using WebBlazorEc.Client;
 global using WebBlazorEc.Shared;
 global using System.Net.Http.Json;
 global using WebBlazorEc.Client.Services.ProductService;
+global using WebBlazorEc.Client.Services.CategoryService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,5 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 await builder.Build().RunAsync();
---
+++ WebBlazorEc/Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,25 @@
+﻿namespace WebBlazorEc.Client.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly HttpClient _http;
+
+        public CategoryService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        //Khởi tạo một danh sách danh mục mới là null
+        public List<Category> Categories { get; set; } = new List<Category>();
+
+        public async Task GetCategories()
+        {
+            var response = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/category");
+            if (response != null && response.Data != null)
+            {
+                Categories = response.Data; // gán dữ liệu lấy về từ response là Data cho categories
+            }
+            
+        }
+    }
+}
