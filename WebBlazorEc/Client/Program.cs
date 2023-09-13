4ce6829ddeed123387594749d51834e629902cf6
Thông Trần <quangthong211101@gmail.com>
2023-09-13 08:43:09 +0700

    Introducing the Cart with the Local Storage

--- WebBlazorEc/Client/Program.cs
+++ WebBlazorEc/Client/Program.cs
@@ -5,10 +5,13 @@ global using WebBlazorEc.Client.Services.ProductService;
 global using WebBlazorEc.Client.Services.CategoryService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
