cf3b293beb9166440453ff5b474106dab0b11496
Thông Trần <quangthong211101@gmail.com>
2023-09-14 16:28:01 +0700

    Add the UserRegister Model

---
+++ WebBlazorEc/Shared/UserRegister.cs
@@ -0,0 +1,9 @@
+﻿namespace WebBlazorEc.Shared
+{
+    public class UserRegister
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
