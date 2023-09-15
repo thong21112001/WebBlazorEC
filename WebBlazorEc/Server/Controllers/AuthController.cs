53c8dbc972da22a28aa12258ee82f23abe04c8a6
Thông Trần <quangthong211101@gmail.com>
2023-09-15 18:42:04 +0700

    Add the AuthController

---
+++ WebBlazorEc/Server/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿namespace WebBlazorEc.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> RegisterUser(UserRegister userRegister)
+        {
+            var response = await _authService.RegisterUser(new User
+            {
+                Email = userRegister.Email,
+            },userRegister.Password);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
