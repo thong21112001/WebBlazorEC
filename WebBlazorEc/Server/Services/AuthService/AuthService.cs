1fa8054d66c0d28f07678da549892bc6aa51ae18
Thông Trần <quangthong211101@gmail.com>
2023-09-15 18:28:42 +0700

    Implement the User Registration on the Server

--- WebBlazorEc/Server/Services/AuthService/AuthService.cs
+++ WebBlazorEc/Server/Services/AuthService/AuthService.cs
@@ -1,4 +1,6 @@
-﻿namespace WebBlazorEc.Server.Services.AuthService
+﻿using System.Security.Cryptography;
+
+namespace WebBlazorEc.Server.Services.AuthService
 {
     public class AuthService : IAuthService
     {
@@ -9,9 +11,26 @@
             _context = context;
         }
 
-        public Task<ServiceResponse<int>> RegisterUser(User user, string password)
+        public async Task<ServiceResponse<int>> RegisterUser(User user, string password)
         {
-            throw new NotImplementedException();
+            if (await UserExits(user.Email))
+            {
+                return new ServiceResponse<int> 
+                {
+                    Success = false,
+                    Message = "User already exits."
+                };
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<int> { Data = user.Id };
         }
 
         //Kiểm tra xem người dùng nào có email này không
@@ -22,6 +41,18 @@
                 return true;
             }
             return false;
+        }
+
+        //Tạo băm mật khẩu
+        private void CreatePasswordHash(string passW, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            //sử dụng thuật toán mã hoá mật khẩu
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(passW));
+
+            }
         }
     }
 }
