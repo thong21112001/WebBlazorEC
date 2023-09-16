fcadc2a88b17158ca87e705b09dca48c2fe9e406
Thông Trần <quangthong211101@gmail.com>
2023-09-16 16:33:52 +0700

    Add a UserLogin model

---
+++ WebBlazorEc/Shared/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBlazorEc.Shared
+{
+    internal class UserLogin
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        
+        [Required, StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
