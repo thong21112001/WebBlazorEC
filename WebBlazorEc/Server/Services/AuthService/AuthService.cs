4dfeea361ad06a58a5febc71a9d8669b1f3c2bdd
PanPan <quangthong211101@gmail.com>
2023-09-18 00:53:26 +0700

    Create the JSON Web Token

--- WebBlazorEc/Server/Services/AuthService/AuthService.cs
+++ WebBlazorEc/Server/Services/AuthService/AuthService.cs
@@ -1,14 +1,20 @@
-﻿using System.Security.Cryptography;
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace WebBlazorEc.Server.Services.AuthService
 {
     public class AuthService : IAuthService
     {
         private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
 
-        public AuthService(DataContext context)
+        public AuthService(DataContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         public async Task<ServiceResponse<string>> Login(string email, string password)
@@ -16,7 +22,7 @@ namespace WebBlazorEc.Server.Services.AuthService
             var respone = new ServiceResponse<string>();
             var user = await _context.Users.FirstOrDefaultAsync(x => x.Email.ToLower().Equals(email.ToLower()));
             
-            if (user != null)
+            if (user == null)
             {
                 respone.Success = false;
                 respone.Message = "User not found.";
@@ -28,7 +34,7 @@ namespace WebBlazorEc.Server.Services.AuthService
             }
             else
             {
-                respone.Data = "token";
+                respone.Data = CreateToken(user);
             }
             return respone;
         }
@@ -73,7 +79,6 @@ namespace WebBlazorEc.Server.Services.AuthService
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(passW));
-
             }
         }
 
@@ -84,6 +89,31 @@ namespace WebBlazorEc.Server.Services.AuthService
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(passW));
                 return computedHash.SequenceEqual(passwordHash);
             }
+        }
+
+        //Hien ma thong bao web json bang Jwt token
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.
+                GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
     }
 }
