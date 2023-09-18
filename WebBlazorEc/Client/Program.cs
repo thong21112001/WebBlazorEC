cb643005f10c38a243d1395ab24aebce53b06db3
PanPan <quangthong211101@gmail.com>
2023-09-18 23:35:01 +0700

    Expose the Authentication State.

--- WebBlazorEc/Client/Program.cs
+++ WebBlazorEc/Client/Program.cs
@@ -6,6 +6,7 @@ global using WebBlazorEc.Client.Services.CategoryService;
 global using WebBlazorEc.Client.Services.CartItemService;
 global using WebBlazorEc.Client.Services.AuthService;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
 
@@ -20,5 +21,8 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 await builder.Build().RunAsync();
