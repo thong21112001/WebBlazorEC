ec981424337a03b07c6ec64be91f3ffe9287c5d7
Thông Trần <quangthong211101@gmail.com>
2023-09-19 16:17:26 +0700

    Implement Changing the Password in the AuthController

--- WebBlazorEc/Server/Controllers/AuthController.cs
+++ WebBlazorEc/Server/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
 ﻿using Azure;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace WebBlazorEc.Server.Controllers
 {
@@ -38,5 +40,19 @@ namespace WebBlazorEc.Server.Controllers
             }
             return Ok(response);
         }
+
+        [HttpPost("change-password"),Authorize]
+        public async Task<ActionResult<ServiceResponse<bool>>> ChangePassword([FromBody] string newPassword)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var response = await _authService.ChangePassword(int.Parse(userId),newPassword);
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
     }
 }
+ 
