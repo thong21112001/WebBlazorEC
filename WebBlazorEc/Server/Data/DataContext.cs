ef04bc943843d5b2f40e520f740766b5b94b2163
Thông Trần <quangthong211101@gmail.com>
2023-09-21 16:25:16 +0700

    Prepare the Order Implementations (Models, Migration & a Composite Key)

--- WebBlazorEc/Server/Data/DataContext.cs
+++ WebBlazorEc/Server/Data/DataContext.cs
@@ -14,6 +14,8 @@
 
             modelBuilder.Entity<ProductVariant>().HasKey(x => new { x.ProductId, x.ProductTypeId });
 
+            modelBuilder.Entity<OrderItem>().HasKey(oi => new { oi.OrderId, oi.ProductId, oi.ProductTypeId });
+
             modelBuilder.Entity<ProductType>().HasData(
                    new ProductType { Id = 1, Name = "Default" },
                    new ProductType { Id = 2, Name = "Paperback" },
@@ -282,5 +284,7 @@
         public DbSet<ProductVariant> ProductVariants { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems  { get; set; }
     }
 }
