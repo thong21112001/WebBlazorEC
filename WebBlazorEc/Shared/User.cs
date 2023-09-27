e1c6c6b7f663b94752731fde54ab29b2df4bb2b7
Thông Trần <quangthong211101@gmail.com>
2023-09-27 16:03:18 +0700

    Add Address Model & New Migration

--- WebBlazorEc/Shared/User.cs
+++ WebBlazorEc/Shared/User.cs
@@ -7,5 +7,6 @@
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public Address Address { get; set; }
     }
 }
