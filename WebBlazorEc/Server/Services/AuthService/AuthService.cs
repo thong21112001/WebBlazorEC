7d2bf9c6c7ced1ac3edc66ab31fb1c81015b9531
Thông Trần <quangthong211101@gmail.com>
2023-09-30 10:43:35 +0700

    Add a Role to the User Model and to the Token in the AuthService

--- WebBlazorEc/Server/Services/AuthService/AuthService.cs
+++ WebBlazorEc/Server/Services/AuthService/AuthService.cs
@@ -107,7 +107,8 @@ namespace WebBlazorEc.Server.Services.AuthService
             List<Claim> claims = new List<Claim>
             {
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(ClaimTypes.Name, user.Email)
+                new Claim(ClaimTypes.Name, user.Email),
+                new Claim(ClaimTypes.Role, user.Role)
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.
--- WebBlazorEc/Shared/User.cs
+++ WebBlazorEc/Shared/User.cs
@@ -8,5 +8,6 @@
         public byte[] PasswordSalt { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public Address Address { get; set; }
+        public string Role { get; set; } = "Customer";
     }
 }
