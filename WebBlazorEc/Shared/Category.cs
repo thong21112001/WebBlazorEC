b051b789112c913d1020b9ef7e79529ebd9ed072
Thông Trần <quangthong211101@gmail.com>
2023-10-01 16:17:37 +0700

    4. Add More Properties to the Category Model

--- WebBlazorEc/Shared/Category.cs
+++ WebBlazorEc/Shared/Category.cs
@@ -1,9 +1,17 @@
-﻿namespace WebBlazorEc.Shared
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebBlazorEc.Shared
 {
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Url { get; set; } = string.Empty;
+        public bool Visible { get; set; } = true;
+        public bool Deleted { get; set; } = false;
+        [NotMapped] //Không phải là cột trong bảng
+        public bool Editing { get; set; } = false;
+        [NotMapped] //Không phải là cột trong bảng
+        public bool IsNew { get; set; } = false;
     }
 }
