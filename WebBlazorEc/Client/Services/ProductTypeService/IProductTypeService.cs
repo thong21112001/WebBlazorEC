c83088afb7c8441a54470ba48d10a1544adc1dac
Thông Trần <quangthong211101@gmail.com>
2023-10-05 10:18:53 +0700

    10. Add ProductTypeService Implementations to the Client

---
+++ WebBlazorEc/Client/Services/ProductTypeService/IProductTypeService.cs
@@ -0,0 +1,9 @@
+﻿namespace WebBlazorEc.Client.Services.ProductTypeService
+{
+    public interface IProductTypeService
+    {
+        event Action OnChange;
+        public List<ProductType> ProductTypes { get; set; }
+        Task GetProductTypeAsync();
+    }
+}
---
+++ WebBlazorEc/Client/Services/ProductTypeService/ProductTypeService.cs
@@ -0,0 +1,22 @@
+﻿namespace WebBlazorEc.Client.Services.ProductTypeService
+{
+    public class ProductTypeService : IProductTypeService
+    {
+        private readonly HttpClient _http;
+
+        public ProductTypeService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<ProductType> ProductTypes { get; set; }
+
+        public event Action OnChange;
+
+        public async Task GetProductTypeAsync()
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<ProductType>>>("api/ProductType");
+            ProductTypes = result.Data;
+        }
+    }
+}
