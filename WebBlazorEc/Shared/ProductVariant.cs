ec8a07deba6fed435048d7a213faa9614aca32da
Thông Trần <quangthong211101@gmail.com>
2023-10-09 10:31:50 +0700

    25. Test (and fix) the Create & Update Implementations

--- WebBlazorEc/Shared/ProductVariant.cs
+++ WebBlazorEc/Shared/ProductVariant.cs
@@ -9,10 +9,10 @@ namespace WebBlazorEc.Shared
         //Không dùng id chính của khoá mà dùng khoá ngoại
         //Sử dụng cái này để không bị lặp vòng tròn: ví dụ như không hiện sản phẩm chính mà hãy hiện biến thể của nó
         [JsonIgnore]    
-        public Product Product { get; set; }
+        public Product? Product { get; set; }
         public int ProductId { get; set; }
         public int ProductTypeId { get; set; }
-        public ProductType ProductType { get; set; }
+        public ProductType? ProductType { get; set; }
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
         [Column(TypeName = "decimal(18,2)")]
